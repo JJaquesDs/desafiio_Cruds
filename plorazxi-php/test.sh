0af7a2da28361689cdd6b265a8ad6ec02167040c
plorazxi <paulo.ss.loraschi@gmail.com>
2025-09-10 21:29:21 -0300

    Alterando a forma de teste da aplicação
    Utilizando conceitos de containers e Docker para isso

--- plorazxi-php/test.sh
+++ plorazxi-php/test.sh
@@ -1,2 +1,15 @@
 !#/bin/bash
-php -S localhost:8080 plorazxi-php/src/main.php
+# Variáveis:
+APP_IMAGE_NAME="app-php-dev:dev-0.1"
+APP_NETWORK_NAME="app-php-network"
+
+# Buildando o projeto
+docker build -t $APP_IMAGE_NAME .
+
+# Criando a network
+docker network create $APP_NETWORK_NAME
+
+# Correndo os containers a partir das imagens, cada uma configurada do jeito necessário
+docker run -d -p 8080:8080 -v .:/usr/local/app --network $APP_NETWORK_NAME --name app-php-cont $APP_IMAGE_NAME
+
+docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD="0000" --network $APP_NETWORK_NAME --name mysql-cont mysql:oraclelinux9
