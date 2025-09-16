3a20d7bb9d86f4683de7eb31f267ab448196552f
plorazxi <paulo.ss.loraschi@gmail.com>
2025-09-16 05:15:35 -0300

    Criando volume em cima do vendor, para impedir de ser excluido pelo host volume

--- plorazxi-php/test.sh
+++ plorazxi-php/test.sh
@@ -1,4 +1,3 @@
-!#/bin/bash
 # Variáveis:
 APP_IMAGE_NAME="app-php-dev:dev-0.1"
 APP_NETWORK_NAME="app-php-network"
@@ -10,6 +9,6 @@ docker build -t $APP_IMAGE_NAME .
 docker network create $APP_NETWORK_NAME
 
 # Correndo os containers a partir das imagens, cada uma configurada do jeito necessário
-docker run -d -p 8080:8080 -v .:/usr/local/app --network $APP_NETWORK_NAME --name app-php-cont $APP_IMAGE_NAME
+docker run -d -p 8080:8080 -v /usr/local/app/vendor -v .:/usr/local/app --network $APP_NETWORK_NAME --name app-php-cont $APP_IMAGE_NAME
 
 docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD="0000" --network $APP_NETWORK_NAME --name mysql-cont mysql:oraclelinux9
