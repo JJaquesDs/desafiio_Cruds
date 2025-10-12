16444317d6256aee42f8d297973648d63da54bab
plorazxi <paulo.ss.loraschi@gmail.com>
2025-10-12 11:41:35 -0300

    mudando a visualização do arquiv, além da imagem do banco utilizado

--- plorazxi-php/dev.sh
+++ plorazxi-php/dev.sh
@@ -9,6 +9,20 @@ docker build -t $APP_IMAGE_NAME .
 docker network create $APP_NETWORK_NAME
 
 # Correndo os containers a partir das imagens, cada uma configurada do jeito necessário
-docker run -d -p 8080:8080 -v /usr/local/app/vendor -v .:/usr/local/app --network $APP_NETWORK_NAME --name app-php-cont $APP_IMAGE_NAME
+docker run -d \
+    -p 8080:8080 \
+    -v /usr/local/app/vendor \
+    -v .:/usr/local/app \
+    --network $APP_NETWORK_NAME \
+    --name app-php-cont \
+    $APP_IMAGE_NAME
 
-docker run -d -p 3306:3306 -e MYSQL_ROOT_PASSWORD="0000" --network $APP_NETWORK_NAME --name mysql-cont mysql:oraclelinux9
+docker run -d \
+    -p 3306:3306 \
+    -e MYSQL_ROOT_PASSWORD="0000" \
+    -e MYSQL_DATABASE="php_api_dev" \
+    -e MYSQL_USER="root" \
+    -e MYSQL_PASSWORD="0000" \
+    --network $APP_NETWORK_NAME \
+    --name mysql-cont \
+    mysql/mysql-server:latest
